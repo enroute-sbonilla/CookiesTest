9094551f6fc72c32f1f05fa02ca2b72960b302c8
enroute-sbonilla <santiago.bonilla@enroutesystems.com>
2018-12-28 11:26:13 -0600

    Including scenarios to test reward page

--- features/page_objects/reward_page.js
+++ features/page_objects/reward_page.js
@@ -5,6 +5,18 @@ class RewardsPage extends Page {
         return ('//*[@id="content"]/div/div/header/h1');
     }
     
+    get rewardOption() {
+        return ('//*[@id="main-menu"]/li[2]/a');
+    }
+
+    get aboutOption() {
+        return ('//*[@id="main-menu"]/li[3]/a');
+    }
+
+    get slackLogo () {
+        return ('/html/body/nav[1]/a');
+    }
+
     open(maximized = true) {
         super.openRewd('');
         if(maximized) super.maximize();
@@ -21,7 +33,12 @@ class RewardsPage extends Page {
     messageValidation(element, message) {
         browser.waitForVisible(element);
         this.assert(browser.element(element).getText().includes(message), true);
-     }
+    }
+
+    goToOption(option) {
+        browser.waitForVisible(option);
+        browser.element(option).click();
+    }
 }
 
 export default new RewardsPage();
